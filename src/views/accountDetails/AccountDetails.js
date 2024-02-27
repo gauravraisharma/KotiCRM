1f1509a8e33de0802f507cb20bbf4e2f81c3762c
Rohitchoudhary-121 <rohit.choudharytechy@gmail.com>
2024-02-27 14:26:14 +0530

    Code Added

---
+++ src/views/accountDetails/AccountDetails.js
@@ -0,0 +1,266 @@
+import React from 'react'
+import {
+  CRow,
+  CCol,
+  CCard,
+  CCardHeader,
+  CButton,
+} from '@coreui/react'
+import { FaAngleLeft } from 'react-icons/fa6'
+import { FaChevronRight } from 'react-icons/fa'
+import "../../css/style.css"
+
+const AccountDetails = () => {
+  return (
+    <CRow>
+      <CCol xs={12}>
+        <CCard className="mb-4">
+          <CCardHeader>
+            <CRow>
+              <CCol xs={6}>Account Name</CCol>
+              <CCol xs={6}>
+                <div className="text-end">
+                  <CButton
+                    style={{ margin: 4 }}
+                    component="input"
+                    type="button"
+                    color="primary"
+                    value="Send Email"
+                  />
+                  <CButton
+                    style={{ margin: 4 }}
+                    component="input"
+                    type="button"
+                    color="light"
+                    value="Edit"
+                  />
+                  <CButton
+                    style={{ margin: 4 }}
+                    component="input"
+                    type="button"
+                    color="light"
+                    value="..."
+                  />
+                  <FaAngleLeft />
+                  <FaChevronRight />
+                </div>
+              </CCol>
+            </CRow>
+          </CCardHeader>
+        </CCard>
+      </CCol>
+      <CCol>
+        <CRow xs={12}>
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button
+                class="nav-link active"
+                id="home-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#home"
+                type="button"
+                role="tab"
+                aria-controls="home"
+                aria-selected="true"
+              >
+                Account Detail
+              </button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button
+                class="nav-link"
+                id="profile-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#profile"
+                type="button"
+                role="tab"
+                aria-controls="profile"
+                aria-selected="false"
+              >
+                Contacts
+              </button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button
+                class="nav-link"
+                id="contact-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#contact"
+                type="button"
+                role="tab"
+                aria-controls="contact"
+                aria-selected="false"
+              >
+                Notes 2
+              </button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button
+                class="nav-link"
+                id="contact-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#contact"
+                type="button"
+                role="tab"
+                aria-controls="contact"
+                aria-selected="false"
+              >
+                Attachments 5
+              </button>
+            </li>
+          </ul>
+          
+        </CRow>
+
+        <CRow>
+          <CCol xs={6}>
+            <h5 style={{ padding: 10 }}>Account information</h5>
+            <ul>
+              <li>
+                <span  className="title">Account owner - </span>
+                <span className="info">Gourav Rai</span>
+              </li>
+              <li>
+                <span className="title">Account Site </span>
+                <span className="info">-</span>
+              </li>
+              <li>
+                <span className="title"> Account Number </span>
+
+                <span className="info">0</span>
+              </li>
+              <li>
+                <span className="title"> Account Type </span>
+
+                <span className="info">Vendor</span>
+              </li>
+              <li>
+                <span className="title"> Annual Revenue </span>
+
+                <span className="info">$850,000,00</span>
+              </li>
+              <li>
+                <span className="title"> Created By </span>
+
+                <span className="info">Gourav rai</span>
+              </li>
+            </ul>
+            <h5 className="title" style={{ padding: 10 }}>
+              Address information
+            </h5>
+
+            <ul>
+              <li>
+                <span className="title"> Billing street</span>
+                <span className="info">228 Runamuck PI#2808</span>
+              </li>
+
+              <li>
+                <span className="title">Billing City </span>
+                <span className="info">Baltimore</span>
+              </li>
+              <li>
+                <span className="title">Billing State </span>
+
+                <span className="info">MD</span>
+              </li>
+              <li>
+                <span className="title"> Billing Code </span>
+                <span className="info">21224</span>
+              </li>
+              <li>
+                <span className="title"> Billing Country </span>
+                <span className="info">United States</span>
+              </li>
+            </ul>
+
+            <h5 className="title" style={{ padding: 10 }}>
+              Discription information
+            </h5>
+
+            <ul>
+                <li>
+              
+                <span>Description</span>
+              
+                King is a multinational electronics contract manufacturing company with its
+                headquarters in Baltimore,United States
+              </li>
+            </ul>
+          </CCol>
+          <CCol xs={6}>
+            <ul>
+              <li>
+                <span className="title"> Rating </span>
+                <span className="info">-</span>
+              </li>
+
+              <li>
+                <span className="title">Phone</span>
+                <span className="info">555-555-5555</span>
+              </li>
+              <li>
+                <span className="title"> Fax </span>
+                <span className="info">-</span>
+              </li>
+              <li>
+                <span className="title">Website </span>
+                <span className="info">https://Kingmanufacturing.com</span>
+              </li>
+              <li>
+                <span className="title">Ownership</span>
+                <span className="info">Partnershil</span>
+              </li>
+              {/* <CButton
+                style={{ margin: 4 }}
+                component="input"
+                type="button"
+                color="secondary"
+                value="Locate Map"
+              /> */}
+              <li>
+                <span className="title">Employees</span>
+                <span className="info">445</span>
+              </li>
+              <li>
+                <span className="title"> SIC code </span>
+                <span className="info">-</span>
+              </li>
+              <li>
+               <span className="title">Modified By</span> 
+                <span className="info">Gourav Rai
+                </span>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <span className="title"> Shipping Street </span>
+                <span className="info">-</span>
+              </li>
+
+              <li>
+                <span className="title"> Shipping City </span>
+                <span className="info">-</span>
+              </li>
+              <li>
+                <span className="title">Shipping State </span>
+                <span className="info">-</span>
+              </li>
+              <li>
+                <span className="title">Shipping Code</span>
+                <span className="info">-</span>
+              </li>
+              <li>
+                <span className="title"> Shipping Country </span>
+                <span className="info">-</span>
+              </li>
+            </ul>
+          </CCol>
+        </CRow>
+      </CCol>
+    </CRow>
+  )
+}
+
+export default AccountDetails
---
+++ src/views/accountsList/AccountList.js
@@ -0,0 +1,143 @@
+import React from 'react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableCaption,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+  CButton,
+  CDropdown,
+  CDropdownMenu,
+  CDropdownItem,
+  CDropdownToggle,
+} from '@coreui/react'
+import { DocsExample } from 'src/components'
+import { FaEdit } from 'react-icons/fa'
+import { MdDelete } from 'react-icons/md'
+import { LuView } from "react-icons/lu";
+
+const AccountList = () => {
+  // Sample data for accounts
+  //   const [accounts, setAccounts] = useState([
+  //     { id: 1, name: 'Account 1', owner: 'Owner 1', phone: '123-456-7890', country: 'USA' },
+  //     { id: 2, name: 'Account 2', owner: 'Owner 2', phone: '234-567-8901', country: 'Canada' },
+  // Add more sample accounts as needed
+  //   ]);
+
+  //   const [searchField, setSearchField] = useState('');
+  //   const [searchBy, setSearchBy] = useState('Name');
+
+  //   const handleChange = (event) => {
+  //     setSearchField(event.target.value);
+  // };
+  // const handleSearchByChange = (event) => {
+  //     setSearchBy(event.target.value);
+  // };
+
+  // const filteredAccounts = accounts.filter(account => {
+  //     if (searchBy === 'Name') {
+  //         return account.name.toLowerCase().includes(searchField.toLowerCase());
+  //     } else if (searchBy === 'Owner') {
+  //         return account.owner.toLowerCase().includes(searchField.toLowerCase());
+  //     } else if (searchBy === 'Phone') {
+  //         return account.phone.includes(searchField);
+  //     } else if (searchBy === 'Country') {
+  //         return account.country.toLowerCase().includes(searchField.toLowerCase());
+  //     }
+  //     return true;
+  // });
+
+  return (
+    <CRow>
+      <CCol xs={12}>
+        <CCard className="mb-4">
+          <CCardHeader>
+            <strong>Accounts</strong> <small>List</small>
+          </CCardHeader>
+          <CCardBody>
+            <CRow>
+              <CCol xs={6}>
+                <div className="input-group">
+                  <CDropdown>
+                    <CDropdownToggle style={{ marginRight: 6 }} color="light">
+                      Select
+                    </CDropdownToggle>
+                    <CDropdownMenu>
+                      <CDropdownItem href="#">Name</CDropdownItem>
+                      <CDropdownItem href="#">Owner</CDropdownItem>
+                      <CDropdownItem href="#">Phone</CDropdownItem>
+                      <CDropdownItem href="#">Country</CDropdownItem>
+                    </CDropdownMenu>
+                  </CDropdown>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search for..."
+                    aria-label="Search for..."
+                  />
+                </div>
+                <br></br>
+              </CCol>
+              <CCol xs={6}>
+                <div className="text-end">
+                  <CButton
+                    component="input"
+                    type="button"
+                    color="primary"
+                    value="Create New Account"
+                  />
+                </div>
+              </CCol>
+            </CRow>
+
+            {/* <DocsExample href="components/table"> */}
+            <CTable>
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Owner</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Phone</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Country</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+              <CTableBody>
+                <CTableRow>
+                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                  <CTableDataCell>Akash</CTableDataCell>
+                  <CTableDataCell>Akash khattra</CTableDataCell>
+                  <CTableDataCell>@9988546234</CTableDataCell>
+
+                  <CTableDataCell>
+                    <FaEdit style={{ color: 'green' }} /> <LuView style={{ color: 'blue' }} />{' '}
+                    <MdDelete style={{ color: 'red' }} />{' '}
+                  </CTableDataCell>
+                </CTableRow>
+                <CTableRow>
+                  <CTableHeaderCell scope="row">2</CTableHeaderCell>
+                  <CTableDataCell>Jacob</CTableDataCell>
+                  <CTableDataCell>Thornton</CTableDataCell>
+                  <CTableDataCell>9944466622</CTableDataCell>
+                  <CTableDataCell>
+                    <FaEdit style={{ color: 'green' }} /> <LuView  style={{ color: 'blue' }} />{' '}
+                    <MdDelete style={{ color: 'red' }} />{' '}
+                  </CTableDataCell>
+                </CTableRow>
+              </CTableBody>
+            </CTable>
+            {/* </DocsExample> */}
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  )
+}
+
+export default AccountList
