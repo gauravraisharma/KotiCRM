575860d0b49858358d426b84357df07fe4f42408
Manish Bhatia <Manishbhatia.techbit@outlook.com>
2024-03-15 22:21:37 +0530

    Redux-Persist implemented

--- koticrm.reactuits/src/redux-saga/store.ts
+++ koticrm.reactuits/src/redux-saga/store.ts
@@ -1,16 +1,35 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import mySaga from './sagas/sagas.ts'
 import rootReducer from './reducers/rootReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+  
+//   const persistedReducer=persistReducer(persistConfig,rootReducer);
+// const sagaMiddleware = createSagaMiddleware();
+
+// // const peristStore = createStore(
+// //     persistedReducer,
+// //      applyMiddleware(sagaMiddleware));
+// // sagaMiddleware.run(mySaga);
+
+
+// const PersistStore=configureStore({
+//             reducer:persistedReducer,
+//             middleware:applyMiddleware(sagaMiddleware)
+// })
+
+// export const store=persistStore(peristStore);
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const sagaMiddleware = createSagaMiddleware();
+let store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+let persistor = persistStore(store);
 
-// const rootReducer = combineReducers({
-//   reducer
-// });
-
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(mySaga);
-
-export default store;
+export { store, persistor, sagaMiddleware };
